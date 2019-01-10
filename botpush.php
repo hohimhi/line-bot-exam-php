ab744111620c66da31ee5158b776068651533f55
hohimhi <46376145+hohimhi@users.noreply.github.com>
2019-01-10 19:17:39 +0700

    Revert "Update botpush.php"
    This reverts commit c4df7b2d0b29b161f778ef82cd669fd5c075a750.

--- botpush.php
+++ botpush.php
@@ -16,7 +16,14 @@
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
         replyMsg($arrayHeader,$arrayPostData);
     }
-else if($message == "ฝันดี9"){
+    #ตัวอย่าง Message Type "Sticker"
+    else if($message == "ฝันดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+else if($message == "ฝันดี110"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา110";
